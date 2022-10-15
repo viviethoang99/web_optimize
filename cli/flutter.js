c03d3b5d29a597bed144c9f3b0f3405c5aeabcad
Vi Hoang <viviethoang9@gmail.com>
2022-10-16 04:05:17 +0700

    Test 17

--- cli/flutter.js
+++ cli/flutter.js
@@ -80,22 +80,37 @@ _flutter.loader = null;
       })
     }
 
-
     _retryCount = 0;
     _loadEntrypoint(entrypointUrl) {
       if (!this._scriptLoaded) {
         this._scriptLoaded = new Promise((resolve, reject) => {
           const promises = Object.keys(jsManifest).filter(key => /main.dart_\d.js/g.test(key)).sort().map(key => `${assetBase}${jsManifest[key]}`).map(this._downloadSplitJs);
           Promise.all(promises).then((values)=>{
-            let completeMainJs = values.join('');
-            var script = document.createElement("script");
-            script.text = completeMainJs;
-            script.type = "application/javascript";
+            const contents = values.join("");
+            const script = document.createElement("script");
+            script.text = contents;
+            script.type = "text/javascript";
 
             this._didCreateEngineInitializerResolve = resolve;
             script.addEventListener("error", reject);
             document.body.appendChild(script);
-          })
+          }).catch(()=>{
+            // console.error("main.dart.js download fail，refresh and try again");
+
+            // retry again
+            if (++this._retryCount > 3) {
+              const element = document.createElement("a");
+              element.href = "javascript:location.reload()";
+              element.style.textAlign = "center";
+              element.style.margin = "50px auto";
+              element.style.display = "block";
+              element.style.color = "#f89800";
+              element.innerText = "加载失败，点击重新请求页面";
+              document.body.appendChild(a);
+            } else {
+              this._loadEntrypoint(entrypointUrl);
+            }
+          });
 
 //          let scriptTag = document.createElement("script");
 //          scriptTag.src = entrypointUrl;
